6417dd0bcaaa866e52266fc116bca3d3c5a597d5
Brian Addicks <brian@addicks.us>
2012-11-30 16:36:41 -0500

    added Send-PaApiQuery, will deprecate Get-PaCustom

--- PaloAlto.psm1
+++ PaloAlto.psm1
@@ -30,12 +30,25 @@
         $WebClient = New-Object System.Net.WebClient
         [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
         Add-Type -AssemblyName System.Management.Automation
+
+        $global:PaConnectionArray = @()
+        $Connection = @{}
+        $ConnectionProps = @("Address","ApiKey","ConnectionString")
+        foreach ($Value in $ConnectionProps) {
+            $Connection.Add($Value,$null)
+        }
     }
 
     PROCESS {
         $user = $cred.UserName.Replace("\","")
         $ApiKey = ([xml]$WebClient.DownloadString("https://$Address/api/?type=keygen&user=$user&password=$($cred.getnetworkcredential().password)"))
         if ($ApiKey.response.status -eq "success") {
+            $CurrentConnection = New-Object PsObject -Property $Connection
+            $CurrentConnection.Address = $Address
+            $CurrentConnection.ApiKey = $ApiKey.response.result.key
+            $CurrentConnection.ConnectionString = "https://$Address/api/?key=$($ApiKey.response.result.key)"
+            #$CurrentConnection
+            $global:PaConnectionArray += $CurrentConnection
             return "https://$Address/api/?key=$($ApiKey.response.result.key)"
         } else {
             Throw "$($ApiKey.response.result.msg)"
@@ -134,10 +147,8 @@ function Get-PaCustom {
         $CustomData = [xml]$WebClient.DownloadString($Url)
         if ($CustomData.response.status -eq "success") {
             if (($action -eq "show") -or ($action -eq "get")) {
-                "$action"
                 return $CustomData
             } else {
-                "not get $action"
                 return $customdata.response.status
             }
         } else {
@@ -579,6 +590,334 @@ function Get-PaObject {
         } else {
             return $Result.response.result."$($ObjectTypes.get_item($ObjectType))".entry
             
+        }
+    }
+}
+
+function Send-PaApiQuery {
+	<#
+	.SYNOPSIS
+		Query Palo Alto for custom data.
+	.DESCRIPTION
+		Query Palo Alto for custom data
+	.EXAMPLE
+        Needs to write some examples
+	.EXAMPLE
+		Needs to write some examples
+	.PARAMETER PaConnectionString
+		Specificies the Palo Alto connection string with address and apikey.
+	#>
+    Param (
+        #############################CONFIG#############################
+
+        [Parameter(ParameterSetName="config",Mandatory=$True,Position=0)]
+        [ValidateSet("show","get","set","edit","delete","rename","clone","move")] 
+        [String]$Config,
+
+        [Parameter(ParameterSetName="config",Mandatory=$True,Position=2)]
+        [ValidatePattern("\/config\/.*")]
+        [String]$XPath,
+
+        [Parameter(ParameterSetName="config")]
+        [alias('e')]
+        [ValidatePattern("<\w+>.*<\/\w+>")]
+        [String]$Element,
+
+        [Parameter(ParameterSetName="config")]
+        [alias('nn')]
+        [String]$NewName,
+
+            #========================CLONE=========================#
+
+        [Parameter(ParameterSetName="config")]
+        [alias('cf')]
+        [String]$CloneFrom,
+
+            #=========================MOVE=========================#
+
+        [Parameter(ParameterSetName="config")]
+        [alias('mw')]
+        [ValidateSet("after","before","top","bottom")] 
+        [String]$MoveWhere,
+
+        [Parameter(ParameterSetName="config")]
+        [alias('dst')]
+        [String]$MoveDestination,
+
+        ###########################OPERATIONAL##########################
+
+        [Parameter(ParameterSetName="op",Mandatory=$True,Position=0)]
+        [ValidatePattern("<\w+>.*<\/\w+>")]
+        [String]$Op,
+
+        #############################REPORT#############################
+
+        [Parameter(ParameterSetName="report",Mandatory=$True,Position=0)]
+        [ValidateSet("dynamic","predefined")]
+        #No Custom Reports supported yet, should probably make a seperate cmdlet for it.
+        [String]$Report,
+
+        [Parameter(ParameterSetName="report")]
+        [alias('rn')]
+        [String]$ReportName,
+
+        [Parameter(ParameterSetName="report")]
+        [alias('r')]
+        [Decimal]$Rows,
+
+        [Parameter(ParameterSetName="report")]
+        [alias('p')]
+        [ValidateSet("last-60-seconds","last-15-minutes","last-hour","last-12-hrs","last-24-hrs","last-calendar-day","last-7-days","last-7-calendar-days","last-calendar-week","last-30-days")] 
+        [String]$Period,
+
+        [Parameter(ParameterSetName="report")]
+        [alias('start')]
+        [ValidatePattern("\d{4}\/\d{2}\/\d{2}\+\d{2}:\d{2}:\d{2}")]
+        [String]$StartTime,
+
+        [Parameter(ParameterSetName="report")]
+        [alias('end')]
+        [ValidatePattern("\d{4}\/\d{2}\/\d{2}\+\d{2}:\d{2}:\d{2}")]
+        [String]$EndTime,
+
+        #############################EXPORT#############################
+
+        [Parameter(ParameterSetName="export",Mandatory=$True,Position=0)]
+        [ValidateSet("application-pcap","threat-pcap","filter-pcap","filters-pcap","configuration","certificate","high-availability-key","key-pair","application-block-page","captive-portal-text","file-block-continue-page","file-block-page","global-protect-portal-custom-help-page","global-protect-portal-custom-login-page","global-protect-portal-custom-welcome-page","ssl-cert-status-page","ssl-optout-text","url-block-page","url-coach-text","virus-block-page","tech-support","device-state")]
+        [String]$Export,
+
+        [Parameter(ParameterSetName="export")]
+        [alias('f')]
+        [String]$From,
+
+        [Parameter(ParameterSetName="export")]
+        [alias('t')]
+        [String]$To,
+
+            #=========================DLP=========================#
+
+        [Parameter(ParameterSetName="export")]
+        [alias('dp')]
+        [String]$DlpPassword,
+
+            #=====================CERTIFICATE=====================#
+
+        [Parameter(ParameterSetName="export")]
+        [alias('ecn')]
+        [String]$CertificateName,
+
+        [Parameter(ParameterSetName="export")]
+        [alias('ecf')]
+        [ValidateSet("pkcs12","pem")]
+        [String]$CertificateFormat,
+
+        [Parameter(ParameterSetName="export")]
+        [alias('epp')]
+        [String]$ExportPassPhrase,
+
+            #=====================TECH SUPPORT====================#
+
+        [Parameter(ParameterSetName="export")]
+        [alias('ta')]
+        [ValidateSet("status","get","finish")]
+        [String]$TsAction,
+
+        [Parameter(ParameterSetName="export")]
+        [alias('j')]
+        [Decimal]$Job,
+
+        [Parameter(ParameterSetName="export",Mandatory=$True)]
+        [alias('ef')]
+        [String]$ExportFile,
+
+
+        #############################IMPORT#############################
+
+        [Parameter(ParameterSetName="import",Mandatory=$True,Position=0)]
+        [ValidateSet("software","anti-virus","content","url-database","signed-url-database","license","configuration","certificate","high-availability-key","key-pair","application-block-page","captive-portal-text","file-block-continue-page","file-block-page","global-protect-portal-custom-help-page","global-protect-portal-custom-login-page","global-protect-portal-custom-welcome-page","ssl-cert-status-page","ssl-optout-text","url-block-page","url-coach-text","virus-block-page","global-protect-client","custom-logo")]
+        [String]$Import,
+
+        [Parameter(ParameterSetName="import",Mandatory=$True,Position=1)]
+        [String]$ImportFile,
+
+            #=====================CERTIFICATE=====================#
+
+        [Parameter(ParameterSetName="import")]
+        [alias('icn')]
+        [String]$ImportCertificateName,
+
+        [Parameter(ParameterSetName="import")]
+        [alias('icf')]
+        [ValidateSet("pkcs12","pem")]
+        [String]$ImportCertificateFormat,
+
+        [Parameter(ParameterSetName="import")]
+        [alias('ipp')]
+        [String]$ImportPassPhrase,
+
+            #====================RESPONSE PAGES====================#
+
+        [Parameter(ParameterSetName="import")]
+        [alias('ip')]
+        [String]$ImportProfile,
+
+            #=====================CUSTOM LOGO======================#
+
+        [Parameter(ParameterSetName="import")]
+        [alias('wh')]
+        [ValidateSet("login-screen","main-ui","pdf-report-footer","pdf-report-header")]
+        [String]$ImportWhere,
+
+        ##############################LOGS##############################
+
+        [Parameter(ParameterSetName="log",Mandatory=$True,Position=0)]
+        [ValidateSet("traffic","threat","config","system","hip-match")]
+        [String]$Log,
+
+        [Parameter(ParameterSetName="log")]
+        [alias('q')]
+        [String]$LogQuery,
+
+        [Parameter(ParameterSetName="log")]
+        [alias('nl')]
+        [ValidateRange(1,5000)]
+        [Decimal]$NumberLogs,
+
+        [Parameter(ParameterSetName="log")]
+        [alias('sl')]
+        [String]$SkipLogs,
+
+        [Parameter(ParameterSetName="log")]
+        [alias('la')]
+        [ValidateSet("get","finish")]
+        [String]$LogAction,
+
+        [Parameter(ParameterSetName="log")]
+        [alias('lj')]
+        [Decimal]$LogJob,
+
+        #############################USER-ID############################
+
+        [Parameter(ParameterSetName="userid",Mandatory=$True,Position=0)]
+        [String]$UserId
+    )
+
+    BEGIN {
+        $WebClient = New-Object System.Net.WebClient
+        [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+        if (!($Global:PaConnectionArray)) {
+            Write-Host -ForegroundColor Red "No connections, use Get-PaConnectionString to create them"
+            return
+        }
+    }
+
+    PROCESS {
+        foreach ($Connection in $Global:PaConnectionArray) {
+            $url += $Connection.ConnectionString
+
+            #############################CONFIG#############################
+            if ($Config) {
+                $ReturnType = "String"
+                $url += "&type=config"
+                $url += "&action=$Config"
+                $url += "&xpath=$xpath"
+                if (($Config -eq "set") -or ($Config -eq "edit")) {
+                    $url += "&element=$Element"
+                } elseif ($Config -eq "rename") {
+                    $url += "&newname=$NewName"
+                } elseif ($Config -eq "clone") {
+                    $url += "/"
+                    $url += "&from=$xpath/$CloneFrom"
+                    $url += "&newname=$NewName"
+                    return "Times out ungracefully as of 11/20/12 on 5.0.0"
+                } elseif ($Config -eq "move") {
+                    $url += "&where=$MoveWhere"
+                    if ($MoveDestination) {
+                        $url += "&dst=$MoveDestination"
+                    }
+                }
+                return [xml]$WebClient.DownloadString($url)
+
+            ###########################OPERATIONAL##########################
+            } elseif ($Op) {
+                $ReturnType = "String"
+                $url += "&type=op"
+                $url += "&cmd=$Op"
+                return [xml]$WebClient.DownloadString($url)
+
+            #############################REPORT#############################
+            } elseif ($Report) {
+                $ReturnType = "String"
+                $url += "&type=report"
+                $url += "&reporttype=$Report"
+                if ($ReportName) { $url += "&reportname=$ReportName" }
+                if ($Rows) { $url += "&topn=$Rows" }
+                if ($Period) {
+                    $url+= "&period=$Period"
+                } elseif ($StartTime) {
+                    $url += "&starttime=$StartTime"
+                    if ($EndTime) { $url += "&starttime=$EndTime" }
+                }
+                return [xml]$WebClient.DownloadString($url)
+        
+            #############################EXPORT#############################
+            } elseif ($Export) {
+                if (($export -eq "filters-pcap") -or ($export -eq "filter-pcap")) {
+                    return "Times out ungracefully as of 11/20/12 on 5.0.0"
+                }
+                $url += "&type=export"
+                $url += "&category=$Export"
+                if ($From) { $url += "&from=$From" }
+                if ($To) { $url += "&to=$To" }
+                if ($DlpPassword) { $url += "dlp-password=$DlpPassword" }
+                if ($CertificateName) {
+                    $url += "&certificate-name=$CertificateName"
+                    $url += "&include-key=no"
+                }
+                if ($CertificateFormat) { $url += "&format=$CertificateFormat" }
+                if ($ExportPassPhrase) {
+                    $url += "&include-key=yes"
+                    $url += "&passphrase=$ExportPassPhrase"
+                }
+                if ($TsAction) { $url += "&action=$TsAction" }
+                if ($Job) { $url += "&job-id=$Job" }
+                $WebClient.DownloadFile($url,$ExportFile)
+                return "File downloaded to $ExportFile"
+
+            #############################IMPORT#############################
+            } elseif ($Import) {
+                $url += "&type=import"
+                $url += "&category=$Import"
+                if ($ImportCertificateName) {
+                    $url += "&certificate-name=$ImportCertificateName"
+                    $url += "&format=$ImportCertificateFormat"
+                    $url += "&passphrase=$ImportPassPhrase"
+                }
+                if ($ImportProfile) { $url += "&profile=$ImportProfile" }
+                if ($ImportWhere) { $url += "&where=$ImportWhere" }
+                return "Currently non-functional, not sure how to do this with webclient"
+
+
+            ##############################LOGS##############################
+            } elseif ($Log) {
+                "type=log"
+                "log-type=$Log"
+                if ($LogQuery) { "query=$($LogQuery.Replace(" ",'%20'))" }
+                if ($NumberLogs) { "nlogs=$NumberLogs" }
+                if ($SkipLogs) { "skip=$SkipLogs" }
+                if ($LogAction) {
+                    "action=$LogAction"
+                    "job-id=$LogJob"
+                }
+                return "Haven't gotten to this yet"
+
+            #############################USER-ID############################
+            } elseif ($UserId) {
+                "type=user-id"
+                "action=set"
+                "file=$UserId"
+                return "Haven't gotten to this yet"
+            }
         }
     }
 }

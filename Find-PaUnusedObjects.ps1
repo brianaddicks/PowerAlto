624a58cbdcc1d04576af1ebcece3e5f716041d7b
Brian Addicks <brian@addicks.us>
2012-12-05 21:28:59 -0500

    Added Get-PaObjectUsage, Find-PaUnusedObject

---
+++ Find-PaUnusedObjects.ps1
@@ -0,0 +1,61 @@
+﻿function Find-PaUnusedObjects {
+    <#
+	.SYNOPSIS
+		Returns Security Ruleset from Palo Alto firewall.
+	.DESCRIPTION
+		Returns Security Ruleset from Palo Alto firewall.
+	.EXAMPLE
+        EXAMPLES!
+	.EXAMPLE
+		EXAMPLES!
+	.PARAMETER PaConnectionString
+		Specificies the Palo Alto connection string with address and apikey.
+	#>
+
+    Param (
+        [Parameter(Mandatory=$False)]
+        [alias('pc')]
+        [String]$PaConnection
+    )
+
+    BEGIN {
+        $ResultObject = @{}
+        $ResultProperties = @("Name","Groups","Security","Nat")
+        foreach ($Value in $ResultProperties) {
+            $ResultObject.Add($Value,$null)
+        }
+
+        Function Process-Query ( [String]$PaConnectionString ) {
+            $ResultTable = @()
+            $Addresses = (Send-PaApiQuery -Config get -xpath "/config/devices/entry/vsys/entry/address" -pc $PaConnectionString).response.result.address.entry
+            $i = 0
+            foreach ($Address in $Addresses) {
+                $Usage = Get-PaObjectUsage $Address.name
+                $CurrentResult = New-Object PsObject -Property $ResultObject
+                $CurrentResult.Name     = $Address.name
+                $CurrentResult.Groups   = ($Usage.Groups | measure).count
+                $CurrentResult.Security = ($Usage.Security | measure).count
+                $CurrentResult.Nat      = ($Usage.NatRules | measure).count
+                $ResultTable += $CurrentResult
+                $i++
+                $Progress = [math]::truncate(($i / $Addresses.count) * 100)
+                Write-Progress -Activity "Scanning Address Usage" -Status "$Progress% complete $i/$($Addresses.count)"-PercentComplete $Progress
+            }
+        return $ResultTable
+        }
+    }
+    
+    PROCESS {
+        if ($PaConnection) {
+            Process-Query $PaConnection
+        } else {
+            if (Test-PaConnection) {
+                foreach ($Connection in $Global:PaConnectionArray) {
+                    Process-Query $Connection.ConnectionString
+                }
+            } else {
+                Throw "No Connections"
+            }
+        }
+    }
+}
---
+++ Get-PaObjectUsage.ps1
@@ -0,0 +1,89 @@
+﻿function Get-PaObjectUsage {
+    <#
+	.SYNOPSIS
+		Returns Security Ruleset from Palo Alto firewall.
+	.DESCRIPTION
+		Returns Security Ruleset from Palo Alto firewall.
+	.EXAMPLE
+        EXAMPLES!
+	.EXAMPLE
+		EXAMPLES!
+	.PARAMETER PaConnectionString
+		Specificies the Palo Alto connection string with address and apikey.
+	#>
+
+    Param (
+        [Parameter(Mandatory=$True,Position=0)]
+        [String]$SearchString,
+
+        [Parameter(Mandatory=$False)]
+        [alias('pc')]
+        [String]$PaConnection
+    )
+
+    BEGIN {
+        $ReturnObject = @{}
+        $ReturnProperties = @("Addresses","Groups","SecurityRules","NatRules")
+        foreach ($Value in $ReturnProperties) {
+            $ReturnObject.Add($Value,$null)
+        }
+        Function Process-Query ( [String]$PaConnectionString ) {
+            $SecurityRuleBase = Get-PaSecurityRule
+            $NatRuleBase = Get-PaNatRule
+            $Objects = Find-PaAddressObject $SearchString
+
+            $SecurityRuleUse = @()
+            $NatRuleUse      = @()
+
+            foreach ($Address in $Objects.Addresses) {
+                $SecurityRuleUse += $SecurityRulebase | where { ($_.SourceAddress -contains $Address.Name) -or $_.DestinationAddress -contains $Address.Name }
+                $NatRuleUse += $NatRuleBase | where { ($_.SourceAddress -contains $Address.name) -or ($_.DestinationAddress -contains $Address.name) -or ($_.SourceTransAddress -contains $Address.name) -or ($_.DestTransAddress -contains $Address.name) }
+            }
+            foreach ($Group in $Objects.Groups) {
+                $SecurityRuleUse += $SecurityRulebase | where { ($_.SourceAddress -contains $Group.name) -or ($_.DestinationAddress -contains $Group.name) }
+                $NatRuleUse += $NatRuleBase | where { ($_.SourceAddress -contains $Group.name) -or ($_.DestinationAddress -contains $Group.name) -or ($_.SourceTransAddress -contains $Group.name) -or ($_.DestTransAddress -contains $Group.name) }
+            }
+
+            ""
+            if ($Objects) {
+                if ($Objects.Addresses) {
+                    #Write-Host "$SearchString is a member of the following $($Objects.Addresses.Count) Address(es)"
+                    #$Objects.Addresses | ft Name,Value -AutoSize
+                    $ReturnObject.Addresses = $Objects.Addresses
+                }
+                if ($Objects.Groups) {
+                    #Write-Host "$SearchString is a member of the following $($Objects.Groups.count) Group(s)"
+                    #$Objects.Groups | ft Name,Member -AutoSize
+                    $ReturnObject.Groups = $Objects.Groups
+                }
+            }
+
+            if ($SecurityRuleUse) {
+                #Write-Host "$SearchString is used in the following $($SecurityRuleUse.count) Security Rules"
+                #$SecurityRuleuse | ft Name,SourceAddress,DestinationAddress -AutoSize
+                $ReturnObject.SecurityRules = $SecurityRuleUse
+            }
+
+            if ($NatRuleUse) {
+                #Write-Host "$SearchString is used in the following $($NatRuleUse.count) Nat Rules"
+                #$NatRuleuse | ft Name,SourceAddress,DestinationAddress -AutoSize
+                $ReturnObject.NatRules = $NatRuleUse
+            }
+        return $ReturnObject
+        }
+    }
+    
+    PROCESS {
+        if ($PaConnection) {
+            Process-Query $PaConnection
+        } else {
+            if (Test-PaConnection) {
+                foreach ($Connection in $Global:PaConnectionArray) {
+                    Process-Query $Connection.ConnectionString
+                }
+            } else {
+                Throw "No Connections"
+            }
+        }
+    }
+}

62ea68c30b843cd433fd11e5ec815eafa589a399
Brian Addicks <brian@addicks.us>
2013-04-03 15:55:31 -0400

    testing out import functions

--- poweralto.psm1
+++ poweralto.psm1
@@ -1096,6 +1096,27 @@ function Send-PaApiQuery {
     )
 
     BEGIN {
+        function Send-WebFile ($url,$data) {
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($data)
+
+            [System.Net.HttpWebRequest] $webRequest = [System.Net.WebRequest]::Create($url)
+
+            $webRequest.Method = "POST"
+            $webRequest.ContentType = "text/html"
+            $webRequest.ContentLength = $buffer.Length;
+
+            $requestStream = $webRequest.GetRequestStream()
+            $requestStream.Write($buffer, 0, $buffer.Length)
+            $requestStream.Flush()
+            $requestStream.Close()
+
+
+            [System.Net.HttpWebResponse] $webResponse = $webRequest.GetResponse()
+            $streamReader = New-Object System.IO.StreamReader($webResponse.GetResponseStream())
+            $result = $streamReader.ReadToEnd()
+            return $result
+        }
+
         Add-Type -AssemblyName System.Web
         $WebClient = New-Object System.Net.WebClient
         [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
@@ -1203,7 +1224,9 @@ function Send-PaApiQuery {
                 if ($ImportProfile) { $url += "&profile=$ImportProfile" }
                 if ($ImportWhere) { $url += "&where=$ImportWhere" }
                 $global:lasturl = $url
-                return "Currently non-functional, not sure how to do this with webclient"
+
+                return Send-WebFile $url $ImportFile
+                #return "Currently non-functional, not sure how to do this with webclient"
 
 
             ##############################LOGS##############################

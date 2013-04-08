38296a49a2cfa53019a07c3eced4468ea847af91
Brian Addicks <brian@addicks.us>
2013-04-08 16:50:06 -0400

    working on log capability

--- poweralto.psm1
+++ poweralto.psm1
@@ -1,4 +1,72 @@
 Import-module ipv4math -global
+function Create-PaLogJob {
+	<#
+	.SYNOPSIS
+		Formulate and send an api query to a PA firewall.
+	.DESCRIPTION
+		Formulate and send an api query to a PA firewall.
+	.PARAMETER PaConnectionString
+		Specificies the Palo Alto connection string with address and apikey. If ommitted, $global:PaConnectionArray will be used
+	#>
+
+    Param (
+        [Parameter(Mandatory=$True)]
+        [ValidateSet("traffic","threat","config","system","hip-match")]
+        [String]$Type,
+
+        [Parameter(Mandatory=$True)]
+        [alias('q')]
+        [String]$Query,
+
+        [Parameter(Mandatory=$False)]
+        [alias('nl')]
+        [ValidateRange(1,5000)]
+        [Decimal]$NumberLogs,
+
+        [Parameter(Mandatory=$False)]
+        [alias('s')]
+        [String]$Skip
+    )
+
+    BEGIN {
+        Add-Type -AssemblyName System.Web
+        $WebClient = New-Object System.Net.WebClient
+
+        Function Process-Query ( [String]$PaConnectionString ) {
+            $url = $PaConnectionString
+
+            $url += "&type=log"
+            $url += "&log-type=$Type"
+
+            if ($Query)      { $Query  = [System.Web.HttpUtility]::UrlEncode($Query)
+                               $url   += "&query=$Query" }
+            if ($NumberLogs) { $url += "&nlogs=$NumberLogs" }
+            if ($Skip)       { $url += "&skip=$SkipLogs" }
+
+            $global:lasturl  = $url
+            $global:response = [xml]$WebClient.DownloadString($url)
+            if ($global:response.response.status -ne "success") {
+                Throw $global:response.response.result.msg
+            }
+
+            return $global:response
+        }
+    }
+
+    PROCESS {
+        if ($PaConnection) {
+            Process-Query $PaConnection
+        } else {
+            if (Test-PaConnection) {
+                foreach ($Connection in $Global:PaConnectionArray) {
+                    Process-Query $Connection.ConnectionString
+                }
+            } else {
+                Throw "No Connections"
+            }
+        }
+    }
+}
 function Find-PaAddressObject {
 	<#
 	.SYNOPSIS
@@ -285,6 +353,61 @@ function Get-PaConnectionString {
     }
 }
 
+function Get-PaLogJob {
+	<#
+	.SYNOPSIS
+		Formulate and send an api query to a PA firewall.
+	.DESCRIPTION
+		Formulate and send an api query to a PA firewall.
+	.PARAMETER PaConnectionString
+		Specificies the Palo Alto connection string with address and apikey. If ommitted, $global:PaConnectionArray will be used
+	#>
+
+    Param (
+        [Parameter(Mandatory=$True)]
+        [ValidateSet("get","finish")]
+        [String]$Action,
+
+        [Parameter(Mandatory=$True)]
+        [alias('j')]
+        [String]$Job
+    )
+
+    BEGIN {
+        Add-Type -AssemblyName System.Web
+        $WebClient = New-Object System.Net.WebClient
+
+        Function Process-Query ( [String]$PaConnectionString ) {
+            $url = $PaConnectionString
+
+            $url += "&type=log"
+            $url += "&action=$Action"
+            $Url += "&job-id=$job"
+
+            $global:lasturl  = $url
+            $global:response = [xml]$WebClient.DownloadString($url)
+            if ($global:response.response.status -ne "success") {
+                Throw $global:response.response.result.msg
+            }
+
+            return $global:response
+        }
+    }
+
+    PROCESS {
+        if ($PaConnection) {
+            Process-Query $PaConnection
+        } else {
+            if (Test-PaConnection) {
+                foreach ($Connection in $Global:PaConnectionArray) {
+                    Process-Query $Connection.ConnectionString
+                }
+            } else {
+                Throw "No Connections"
+            }
+        }
+    }
+}
 function Get-PaNatRule {
     <#
 	.SYNOPSIS
